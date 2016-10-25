694381d1b90651e180ad5cfbf71b5b716a869f11
hou <5466315@qq.com>
2016-10-25 11:11:16 +0800

    es6 baber

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack')
+var path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules)/,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+}
